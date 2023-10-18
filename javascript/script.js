1a0470391511133148815caa8f10fed59d6e67c3
victoria-del <83675728+victoria-del@users.noreply.github.com>
2023-10-17 21:36:01 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,48 @@
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
+let navbar = document.querySelector('.header .flex .navbar');
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+   profile.classList.remove('active');
+}
+
+let mainImage = document.querySelector('.quick-view .box .row .image-container .main-image img');
+let subImages = document.querySelectorAll('.quick-view .box .row .image-container .sub-image img');
+
+subImages.forEach(images =>{
+   images.onclick = () =>{
+      src = images.getAttribute('src');
+      mainImage.src = src;
+   }
+});
